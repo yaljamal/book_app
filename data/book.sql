521236786dce4bd04c780a0886d608d4fd392a4e
yaljamal <yazan.aljamal1@gmail.com>
2020-04-13 23:16:54 +0300

    add lab 12

---
+++ data/book.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books
+(
+    id SERIAL PRIMARY KEY,
+    author VARCHAR(255),
+    title VARCHAR(255),
+    isbn VARCHAR(255),
+    img_url VARCHAR(255),
+    description TEXT,
+    bookshelf VARCHAR(255)
+);
+
+INSERT INTO books
+    (title,img_url,author,description)
+VALUES
+    ('test', 'test', 'Razan', 'sdjsdvdscvsdc');
--- server.js
+++ server.js
@@ -1,27 +1,38 @@
 'use strict';
-
 require('dotenv').config();
 const express = require('express');
-
+const pg = require('pg');
 const PORT = process.env.PORT || 3030;
 const app = express();
 const superagent = require('superagent');
-
 app.use(express.static('./public'));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.set('view engine', 'ejs');
+const client = new pg.Client(process.env.DATABASE_URL);
 
 app.get('/', (req, res) => {
-    res.render('pages/index');
+    res.redirect('/index')
 })
+app.get('/index', (req, res) => {
+    let SQL = 'SELECT * FROM books;';
+    return client.query(SQL)
+        .then(books => {
+            res.render('pages/index', { books: books });
+        })
+});
+
+app.get('/search', (req, res) => {
+    res.render('./serches/new');
+})
+
 app.post('/searches', (req, res) => {
 
     var url;
     if (req.body.serchtype === 'title') {
-        url = `https://www.googleapis.com/books/v1/volumes?q=${req.body.bookname}&intitle=${req.body.bookname}`;
+        url = `https://www.googleapis.com/books/v1/volumes?q=${req.body.bookname}&intitle:${req.body.bookname}`;
         console.log('title Url :', url);
 
     }
@@ -40,11 +51,36 @@ app.post('/searches', (req, res) => {
         });
 
 });
+
+app.get('/books/:book_id', getBookDetails);
+function getBookDetails(req, res) {
+    let SQL = 'SELECT * FROM books WHERE id=$1;';
+    let values = [req.params.book_id];
+    return client.query(SQL, values)
+        .then(result => {
+            res.render('pages/book/detail', { details: result.rows[0] });
+        })
+}
+app.post('/books', insertDetails);
+function insertDetails(req, res) {
+    let { title, author, isbn, image_url, description } = req.body;
+    let SQL = 'INSERT INTO books (title,author,isbn,image_url,description) VALUES ($1,$2,$3,$4,$5);';
+    let saveValues = [itle, author, isbn, image_url, description];
+    return client.query(SQL, saveValues)
+        .then(() => {
+            res.redirect('../index');
+
+        })
+}
+
 function Book(book) {
     this.title = book.volumeInfo.title;
     this.smallThumbnail = book.volumeInfo.imageLinks.smallThumbnail;
     this.authors = book.volumeInfo.authors;
     this.description = book.volumeInfo.description;
+    this.bookshelf = book.volumeInfo.bookshelf;
+    this.isbn = book.volumeInfo.industryIdentifiers[0].type;
+
 
 }
 
